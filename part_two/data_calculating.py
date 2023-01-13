7844954e89280c9af10214e754d5ab133484859e
HStakhiv <gsstahiv@gmail.com>
2023-01-13 19:28:12 +0200

    Add mode calculating functions

--- part_two/data_calculating.py
+++ part_two/data_calculating.py
@@ -40,11 +40,31 @@ def median_calculating(data: list[int]) -> int | float:
     if data_set_len % 2:
         return sorted(data)[index]
 
-    return sum(sorted(data)[index - 1:index + 1]) / 2
+    return sum(sorted(data)[index - 1 : index + 1]) / 2
 
 
 def statistic_median_calculating(data: list[int]) -> int | float:
     return statistics.median(data)
+
+
+def mode_calculating(data: list[int]) -> list:
+    seen_dict = dict()
+
+    for item in data:
+        if item in seen_dict:
+            seen_dict[item] += 1
+        else:
+            seen_dict[item] = 1
+
+    return [
+        key
+        for key in seen_dict.keys()
+        if seen_dict[key] == max(seen_dict.values())
+    ]
+
+
+def statistic_mode_calculating(data: list[int]) -> int:
+    return statistics.mode(data)
 
 
 if __name__ == "__main__":
@@ -52,3 +72,5 @@ if __name__ == "__main__":
     print(statistic_mean_calculating(DATA, ROUND_DIGITS))
     print(median_calculating(DATA))
     print(statistic_median_calculating(DATA))
+    print(mode_calculating(DATA))
+    print(statistic_mode_calculating(DATA))
